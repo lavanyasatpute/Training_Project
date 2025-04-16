ed8ebcc47465c546bddceb240bf829331e8257de
Lavanya.Satpute@jibe.co.in <Lavanya.Satpute@jibe.co.in>
2025-04-16 12:08:01 +0530

    add midlleware

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -9,14 +9,29 @@ import { UserController } from "./controllers/user.controller";
 import { errorMiddleware } from "./middleeware/error.middleware";
 import cors from 'cors'
 import dotenv from 'dotenv';
+import cookiesParser from 'cookie-parser';
 dotenv.config();
 
 const userController = new UserController();
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true // it is used to allow the tokens or header data in a request so thats why we are used for that.
+}))
 // Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({
+    limit:'16kb'
+}));
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:'16kb'
+}));
+
+app.use(express.static("public"));
+
+app.use(cookiesParser())
 // Error handling middleware should be placed after all routes
 app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     errorMiddleware(err, _req, res, next)
@@ -24,6 +39,8 @@ app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
 
 AppDataSource.initialize()
     .then(() => {
+        // console.log(AppDataSource);
+        
         console.log("Database connected successfully!");
     })
     .catch((error) => {
@@ -37,7 +54,7 @@ app.use("/api/users", userRoutes); // Routes for user management
 app.use("/api/feedback", feedbackRoutes); // Routes for feedback management
 app.use("/api/tickets", ticketRoutes); // Routes for ticket management
 app.use("/api/events", eventRoutes); // Routes for event management
-app.use("/api/eventuser", eventuserRoutes)
+app.use("/api/eventuser", eventuserRoutes) //Routes for ralation between user and event
 
 // Root endpoint
 app.get("/", (req, res) => {
